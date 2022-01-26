395ecb839b40240611b40b6043123297043bb563
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-01-26 10:04:34 +0100

    Add files via upload

---
+++ Binary to Decimal Converter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_to_Decimal_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numeri_binari = new double[8];
+            double numero_convertito = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write("Inserire un numero binario: ");
+                do { } while ((double.TryParse(Console.ReadLine(), out numeri_binari[i]) && !(numeri_binari[i] == 0 || numeri_binari[i] == 1)));
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                if (numeri_binari[i] == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    numero_convertito += Math.Pow(2, i);
+                }
+            }
+            Console.WriteLine(numero_convertito);
+
+            Console.ReadKey();
+        }
+    }
+}
